1604198b3b70d147bc9913d2942614f59c6cbc76
Alexandre Devos <a.devos@octocorn.fr>
2023-12-13 11:02:53 +0100

    feat: demo Salarie Heritage :sparkles:

--- src/main/java/fr/octocorn/Main.java
+++ src/main/java/fr/octocorn/Main.java
@@ -1,7 +1,15 @@
 package fr.octocorn;
 
+
+import fr.octocorn.exercices.tp2.Thermometre;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Thermometre thermometreDeCorrinne = new Thermometre();
+        Thermometre autreThermometreDeCorrinne = new Thermometre(12);
+
+        System.out.println("Le thermomètre de ces animaux d'anglais affiche : " + thermometreDeCorrinne.getFahrenheit());
+
+        System.out.println("Le thermomètre des gens civilisés affiche : " + autreThermometreDeCorrinne.getCelsius());
     }
 }
---
+++ src/main/java/fr/octocorn/demos/heritage/Main.java
@@ -0,0 +1,17 @@
+package fr.octocorn.demos.heritage;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Salarie michel = new Salarie(
+                "Quezak",
+                "Michel",
+                LocalDate.of(1980, 12, 31),
+                30,
+                LocalDate.now()
+        );
+
+        System.out.println(michel);
+    }
+}
